56a3664bfdfbfcf750c2e42e121055f755ea57e2
samuelsteven0902 <samuelsteven@student.uns.ac.id>
2022-01-20 13:53:00 +0700

    menambahkan css dan merapikan , menambah file js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ===== SHOW SCROLL ====
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-Up');
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
