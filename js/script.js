eb8cb241883f7601d11f4a9d98adf3cf41588c98
samuelsteven0902 <samuelsteven@student.uns.ac.id>
2022-01-20 15:01:55 +0700

    menambahkan fitur

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ function scrollUp() {
     else scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp)
+
+
+// ==== HOVER =====
+const services = document.getElementById('kartu')
+$(document).ready(function(e) {
+    $('services').on('mouseenter', function(e) {
+        x = e.pageX - $this(this).offset().left;
+        y = e.pageY - $this(this).offset().top;
+        $(this).find('span').css({ top: y, left: x })
+    })
+})
