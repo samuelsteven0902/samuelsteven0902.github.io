04093c5b034767c72a56654c6acd56c505401533
Samuelsteven_ph <samuelstev0902@Gmail.com>
2022-01-21 10:06:28 +0700

    menambahkan css pada section about

--- js/script.js
+++ js/script.js
@@ -9,10 +9,3 @@ window.addEventListener('scroll', scrollUp)
 
 // ==== HOVER =====
 const services = document.getElementById('kartu')
-$(document).ready(function(e) {
-    $('services').on('mouseenter', function(e) {
-        x = e.pageX - $this(this).offset().left;
-        y = e.pageY - $this(this).offset().top;
-        $(this).find('span').css({ top: y, left: x })
-    })
-})
